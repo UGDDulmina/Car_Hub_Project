d7553f09003ea7a0d3ed866e0e7b837f0e5fa86e
UGDDulmina <ug.dinuja@gmail.com>
2025-02-13 23:20:03 +0530

    Initial commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import CustomButton from './CustomButton';
+import footer from './footer';
+import Hero from './Hero';
+import navbar from './navbar';
+
+export {
+    Hero,
+    CustomButton,
+    footer,
+    navbar
+}
